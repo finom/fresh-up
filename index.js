04a3a96447cffe09d2afbd0c452947dcd91b88a2
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-15 17:44:34 +0300

    fix: Eslint errors

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
+
 const watchers = {};
 
 module.exports = (modulePath) => {
-    if(!watchers[modulePath]) {
+    if (!watchers[modulePath]) {
         watchers[modulePath] = fs.watch(modulePath, (eventType) => {
             if (eventType === 'change') {
                 delete require.cache[modulePath];
                 delete watchers[modulePath];
                 watchers[modulePath].close();
             }
-        });;
+        });
     }
-
 };
