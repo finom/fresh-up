3a8f89b5145d452d06af45c9c53d70aa3923b7b9
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-15 17:43:21 +0300

    fix: Reverse prev change

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const watchers = {};
-const watchHandler = (eventType, modulePath) => {
-    if (eventType === 'change') {
-        delete require.cache[modulePath];
-        delete watchers[modulePath];
-        watchers[modulePath].close();
-    }
-};
 
 module.exports = (modulePath) => {
     if(!watchers[modulePath]) {
-        watchers[modulePath] = fs.watch(modulePath, watchHandler);
+        watchers[modulePath] = fs.watch(modulePath, (eventType) => {
+            if (eventType === 'change') {
+                delete require.cache[modulePath];
+                delete watchers[modulePath];
+                watchers[modulePath].close();
+            }
+        });;
     }
+
 };
