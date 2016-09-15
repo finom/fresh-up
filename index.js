43acc6b43bc84e468e97fbfdf0c32c4e70946003
Andrey Gubanov (his digital copy) <andrey.a.gubanov@gmail.com>
2016-09-15 17:51:15 +0300

    fix: Don't remove watcher

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ module.exports = (modulePath) => {
     if (!watchers[modulePath]) {
         watchers[modulePath] = fs.watch(modulePath, (eventType) => {
             if (eventType === 'change') {
-                watchers[modulePath].close();
                 delete require.cache[modulePath];
-                delete watchers[modulePath];
             }
         });
     }
